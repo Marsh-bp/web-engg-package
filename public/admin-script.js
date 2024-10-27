b7a2461bf8631f45e43c0ac5cd445ab7f5e963ee
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-27 20:16:01 +0530

    Update admin-script.js

--- public/admin-script.js
+++ public/admin-script.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const blockedIPsList = document.getElementById('blockedIPsList');
-    const blockedIPs = JSON.parse(localStorage.getItem('blockedIPs')) || [];
 
-    if (blockedIPs.length === 0) {
-        blockedIPsList.innerHTML = '<li>No blocked IP addresses</li>';
-    } else {
-        blockedIPs.forEach(ip => {
-            const li = document.createElement('li');
-            li.textContent = ip;
-            blockedIPsList.appendChild(li);
-        });
-    }
+    fetch('/api/get-blocked-ips')
+        .then(response => response.json())
+        .then(data => {
+            const blockedIPs = data.blockedIPs;
+
+            if (blockedIPs.length === 0) {
+                blockedIPsList.innerHTML = '<li>No blocked IP addresses</li>';
+            } else {
+                blockedIPs.forEach(ip => {
+                    const li = document.createElement('li');
+                    li.textContent = ip;
+                    blockedIPsList.appendChild(li);
+                });
+            }
+        })
+        .catch(error => console.error('Error fetching blocked IPs:', error));
 });
