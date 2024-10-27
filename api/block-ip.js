81685ba51e0170fdfb6501457bc87a12bedb43cc
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-27 17:00:08 +0530

    Update block-ip.js

--- api/block-ip.js
+++ api/block-ip.js
@@ -4,12 +4,14 @@ let blockedIps = new Set();
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { ip } = req.body;
-    
-    // Block the IP
-    blockedIps.add(ip);
-    console.log(`Blocked IP: ${ip}`);
-    
-    return res.status(200).json({ message: `IP ${ip} blocked` });
+    if (ip) {
+      // Block the IP
+      blockedIps.add(ip);
+      console.log(`Blocked IP: ${ip}`);
+      return res.status(200).json({ message: `IP ${ip} blocked` });
+    } else {
+      return res.status(400).json({ message: 'IP address is required' });
+    }
   } else {
     return res.status(405).json({ message: 'Method not allowed' });
   }
