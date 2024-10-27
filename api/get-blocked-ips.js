d538cc95352932e6b10b7ba0cf32bae8e01555fe
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-27 21:02:11 +0530

    Update get-blocked-ips.js

--- api/get-blocked-ips.js
+++ api/get-blocked-ips.js
@@ -1,22 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const { MongoClient } = require('mongodb');
 
-exports.handler = async (event, context) => {
-    const filePath = path.join(__dirname, '..', 'blocked-ips.txt');
+const uri = "mongodb+srv://bharathpranesh2004:Marsh_bp2004@cluster0.noiu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf-8', (err, data) => {
-            if (err) {
-                resolve({
-                    statusCode: 500,
-                    body: JSON.stringify({ error: 'Failed to read file' })
-                });
-            }
-            const ips = data.trim().split('\n').filter(Boolean);
-            resolve({
-                statusCode: 200,
-                body: JSON.stringify({ blockedIPs: ips })
-            });
-        });
-    });
+exports.handler = async (event) => {
+    try {
+        await client.connect();
+        const database = client.db('IP'); 
+        const collection = database.collection('blockedIPs'); 
+        
+        const blockedIPs = await collection.find({}).toArray();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ blockedIPs: blockedIPs.map(ip => ip.ip) })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to fetch blocked IPs' })
+        };
+    } finally {
+        await client.close();
+    }
 };
