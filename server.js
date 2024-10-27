97dca31e0e891c1b69ec8917672d916b66a12a2f
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-27 18:09:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/get-ip', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.json({ ip });
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
