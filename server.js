cf0ac83c69a89d7622522040c6a6f8a0e9699d40
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-27 19:38:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/api/get-ip', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.json({ ip: ip });
 });
 
-app.use(express.static('public'));
-
 app.listen(3000, () => {
-    console.log(`Server running`);
+    console.log('Server running');
 });
