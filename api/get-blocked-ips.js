0a2b2cad17e6b6d6f7b3cc2522fa2866d10d1aeb
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-27 21:23:00 +0530

    Update get-blocked-ips.js

--- api/get-blocked-ips.js
+++ api/get-blocked-ips.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://bharathpranesh2004:Marsh_bp2004@cluster0.noiu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+const uri = process.env.MONGODB_URI; 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 exports.handler = async (event) => {
     try {
         await client.connect();
         const database = client.db('IP'); 
-        const collection = database.collection('blockedIPs'); 
+        const collection = database.collection('blockedIPs');
         
         const blockedIPs = await collection.find({}).toArray();
 
