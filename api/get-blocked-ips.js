51ebf84c91f513427a7a85f7f7adbdd9bd7099ea
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-10-28 10:26:19 +0530

    Update get-blocked-ips.js

--- api/get-blocked-ips.js
+++ api/get-blocked-ips.js
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch');
 
 const GIST_ID = '09ad334f546c906896ec7b93621ae643';
-const GIST_TOKEN = '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'; 
-
+const GIST_TOKEN = process.env.GIST_TOKEN
 exports.handler = async (event) => {
     const response = await fetch(`https://api.github.com/gists/${GIST_ID}`, {
         headers: {
